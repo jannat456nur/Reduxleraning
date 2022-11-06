b7e731ff10992d9c5d949766eac0805099fbcdc7
jannat456nur <jannatjenny22@gmail.com>
2022-11-06 11:47:45 +0600

    tab 2

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,25 @@
-import { StyleSheet } from 'react-native';
+import { Pressable, StyleSheet } from "react-native";
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
+import { useDispatch,useSelector } from "react-redux";
+import { increment, decrement, selectCount } from "../store/counterSlice";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+  const dispatch = useDispatch();
+  const count =(useSelector(selectCount))
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text style={styles.title}>{count}</Text>
+      <Pressable onPress={() => dispatch(increment())}>
+        <Text>Increment</Text>
+      </Pressable>
+      <Pressable onPress={() => dispatch(decrement())}>
+        <Text>decrement</Text>
+      </Pressable>
     </View>
   );
 }
@@ -17,16 +27,16 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
